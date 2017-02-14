6412d69d2d0b2bd07673acded626875f74f05028
Addison Higham <ahigham@instructure.com>
2017-02-14 14:29:46 -0700

    add session token env vars

--- modules/scala/files/build_docker.sh
+++ modules/scala/files/build_docker.sh
@@ -47,5 +47,7 @@ echo "running docker container to build and upload lambda"
 docker run \
   -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
   -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+  -e AWS_SECURITY_TOKEN=${AWS_SECURITY_TOKEN} \
+  -e AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN} \
   lambda_builder_${lambdaName} || die "failed to run image"
 rm -rf $mytmpdir
