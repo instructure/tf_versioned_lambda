b82a9157f9c031cb4f045f2e53b633d019b49e5f
Jacob Burroughs <jburroughs@instructure.com>
2020-03-06 13:53:42 -0600

    Pass region through to builder

--- modules/node_build/files/build_docker.sh
+++ modules/node_build/files/build_docker.sh
@@ -46,5 +46,7 @@ docker run \
   -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
   -e AWS_SECURITY_TOKEN="$AWS_SECURITY_TOKEN" \
   -e AWS_SESSION_TOKEN="$AWS_SESSION_TOKEN" \
+  -e AWS_REGION="$AWS_REGION" \
+  -e AWS_DEFAULT_REGION="$AWS_DEFAULT_REGION" \
   "lambda_builder_$lambdaName" || die "failed to run image"
 rm -rf "$mytmpdir"
